64901968ef16040e0b38ec3daa62d2dda002021b
holaexe <gussybussy200@gmail.com>
2025-08-14 11:15:15 +1200

    ip_lookup.py

--- ip_lookup.py
+++ ip_lookup.py
@@ -1,7 +1,28 @@
 import requests
+import os
+import time
+from colorama import Fore
+
+def clear():
+    if os.name  == 'nt': 
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def ascii():
+    ascii = f""" {Fore.BLUE}
+    
+
+ @@@ @@@@@@@           @@@       @@@@@@   @@@@@@  @@@  @@@ @@@  @@@ @@@@@@@ 
+ @@! @@!  @@@          @@!      @@!  @@@ @@!  @@@ @@!  !@@ @@!  @@@ @@!  @@@
+ !!@ @!@@!@!           @!!      @!@  !@! @!@  !@! @!@@!@!  @!@  !@! @!@@!@! 
+ !!: !!:               !!:      !!:  !!! !!:  !!! !!: :!!  !!:  !!! !!:     
+ :    :       .......  : ::.: :  : :. :   : :. :   :   :::  :.:: :   :      
+              : :: : :                                                      """
+    print(ascii)
 
 def ip_lookup():
-    ip = input("Enter Target IP >> ")
+    ip = input("\n\nEnter Target IP >> ")
     output_option = input("Do you want to store results into a output.txt? [Y or N] >> ")
     output_normal = output_option.strip().lower()
     try:
@@ -27,7 +48,20 @@ def ip_lookup():
             o.write(str(data) + "\n")
     return data
 
+def main():
+    clear()
+    ascii()
+    print("                     [1] IP Lookup")
+    option = input("Option >> ")
+    if option == '1':
+        ip_lookup()
+    else:
+        print("Returning...")
+        time.sleep(0.5)
+        main()
+
+input("Press Enter to return")
+main()
 final = ip_lookup()
 if __name__ == '__main__':
     print(final)
-    input("Press Enter to Exit...")
